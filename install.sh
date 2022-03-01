6c86022469915d38fd1bd4c4811abcd4d412281a
sam0402 <sam0402@gmail.com>
2022-03-01 09:18:35 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -281,9 +281,11 @@ arch-chroot /mnt wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/
 arch-chroot /mnt pacman -U --noconfirm /root/squeezelite-1.9.8.1317-dsd-x86_64.pkg.tar.zst
 sed -i '/ExecStart=/iExecStartPre=/usr/bin/sleep 3' /mnt/usr/lib/systemd/system/squeezelite.service
 [ $cpus -ge 4 ] && sed -i '/ExecStart=/iType=idle\nExecStartPost=/usr/bin/taskset -cp 3 $MAINPID' /mnt/usr/lib/systemd/system/squeezelite.service
-sed -i 's/^AUDIO_DEV="-o .*/AUDIO_DEV="-o '"$scard"'"/' /mnt/etc/squeezelite.conf
 
-    [[ $player =~ S ]] && arch-chroot /mnt systemctl enable squeezelite
+if  [[ $player =~ S ]]; then
+    sed -i 's/^AUDIO_DEV="-o .*/AUDIO_DEV="-o '"$scard"'"/' /mnt/etc/squeezelite.conf
+    arch-chroot /mnt systemctl enable squeezelite
+fi
 if  [[ $player =~ A ]]; then
     echo Install Airplay ...
     arch-chroot /mnt wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/shairport-sync-3.3.9-1-x86_64.pkg.tar.zst
