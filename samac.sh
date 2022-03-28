e67111cf94a95e29c2a164fb73c115f54c9d2824
sam0402 <sam0402@gmail.com>
2022-03-28 20:29:18 +0800

    Update samac.sh

--- samac.sh
+++ samac.sh
@@ -156,7 +156,6 @@ case $server in
 esac
 
 cpu=intel; cat /proc/cpuinfo | grep -q AMD && cpu=amd
-rm /mnt/boot/intel-ucode.img
 pacstrap /mnt base linux linux-firmware ${cpu}-ucode grub efibootmgr gptfdisk f2fs-tools xfsprogs networkmanager openssh dhclient \
          vim nano wget avahi sudo dialog cpupower lm_sensors
 genfstab -Up /mnt | sed '/^$/d' >>/mnt/etc/fstab
