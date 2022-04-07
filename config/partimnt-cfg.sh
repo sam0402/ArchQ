a595d045e2c7c020f613bcaa61a95bde388309cf
sam0402 <sam0402@gmail.com>
2022-04-07 22:02:35 +0800

    Update partimnt-cfg.sh

--- config/partimnt-cfg.sh
+++ config/partimnt-cfg.sh
@@ -38,6 +38,7 @@ case $WK in
         echo "UUID=${ID} /mnt/${MP} $FS ${OP} 0 0" >>$config
         echo "Add $partition ($FS) to /mnt/$MP mount point."
         [ -d "/mnt/${MP}" ] && mount /mnt/${MP} && echo and mounting.
+        systemctl daemon-reload
         ;;
     E)
         MENU=''
