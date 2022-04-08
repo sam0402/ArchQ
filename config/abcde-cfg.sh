66e0cbeccbb6d7e86ba3d336a6b06d34d2b2a50c
sam0402 <sam0402@gmail.com>
2022-04-08 20:08:14 +0800

    Add files via upload

---
+++ config/abcde-cfg.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+config='/etc/abcde.conf'
+if [ ! -f $config ]; then
+    pacman -S --noconfirm glyr cdparanoia libdiscid atomicparsley make
+    pacman -Syu --noconfirm
+
+    wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/abcde-2.9.3-5-any.pkg.tar.zst
+    wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/cd-discid-1.4-3-x86_64.pkg.tar.zst
+    wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/rc-local-4-1-any.pkg.tar.zst
+    pacman -U --noconfirm /root/cd-discid-1.4-3-x86_64.pkg.tar.zst /root/abcde-2.9.3-5-any.pkg.tar.zst /root/rc-local-4-1-any.pkg.tar.zst
+    curl -sL /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/abcde.conf >/etc/abcde.conf
+    echo 'yes' | cpan -fi MusicBrainz::DiscID WebService::MusicBrainz
+
+    sed -i '$d' /etc/rc.local
+cat >>/etc/rc.local <<EOF 
+[ \$(uname -r | awk -F - '{print \$3}') = 'Qrip' ] && \\
+    systemctl stop roonserver squeezelite mpd logitechmediaserver squeezelite shairport-sync 2> /dev/null
+exit 0
+EOF
+fi
+
+while read line; do
+    eval $(grep '=' | grep -v '#')
+done < $config
+
+options=$(dialog --stdout \
+    --title "abcde" \
+    --ok-label "Ok" \
+    --form "Modify settings" 0 40 0 \
+    "Output directory"  1 1   "${OUTPUTDIR}"    1 18 40 0 \
+    "Type (wav/flac)"   2 1   "${OUTPUTTYPE}"   2 18 40 0 \
+    "Read offset"       3 1   "$OFFSET"         3 18 40 0 \
+    "Eject CD (y/n)"    4 1   "$EJECTCD"        4 18 40 0) || exit 1
+clear
+
+OUTPUTDIR=$(echo $options |  awk '//{print $1 }')
+OUTPUTTYPE=$(echo $options |  awk '//{print $2 }')
+OFFSET=$(echo $options |  awk '//{print $3 }')
+EJECTCD=$(echo $options |  awk '//{print $4 }')
+
+[ -z $OUTPUTDIR ] && echo "Fail! Output directory is null." && exit 1
+[ -z $OUTPUTTYPE ] && echo "Fail! Output type is null." && exit 1
+[ -z $OFFSET ] && echo "Fail! Read offset is null." && exit 1
+[ -z $EJECTCD ] && echo "Fail! Eject CD is null." && exit 1
+
+OUTPUTDIR=$(echo $OUTPUTDIR | sed 's"/"\\\/"g')
+sed -i 's/^#\?OUTPUTDIR=".*/OUTPUTDIR="'"$OUTPUTDIR"'"/' $config
+sed -i 's/^#\?OUTPUTTYPE=".*/OUTPUTTYPE="'"$OUTPUTTYPE"'"/' $config
+sed -i 's/^#\?OFFSET=".*/OFFSET="'"$OFFSET"'"/' $config
+sed -i 's/^#\?EJECTCD=.*/EJECTCD='"$EJECTCD"'/' $config
--- config/update_scpt.sh
+++ config/update_scpt.sh
@@ -1,11 +1,12 @@
 rm -f /root/*.tar.zst /root/*.tar.xz*
-curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/shairport-cfg.sh >/usr/bin/shairport-cfg.sh
-curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/sqzlite-cfg.sh >/usr/bin/sqzlite-cfg.sh
-curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/nfs-cfg.sh >/usr/bin/nfs-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/ether-cfg.sh >/usr/bin/ether-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/kernel-cfg.sh >/usr/bin/kernel-cfg.sh
+curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/mpd-cfg.sh >/usr/bin/mpd-cfg.sh
+curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/nfs-cfg.sh >/usr/bin/nfs-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/partimnt-cfg.sh >/usr/bin/partimnt-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/player-cfg.sh >/usr/bin/player-cfg.sh
-curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/mpd-cfg.sh >/usr/bin/mpd-cfg.sh
+curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/shairport-cfg.sh >/usr/bin/shairport-cfg.sh
+curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/sqzlite-cfg.sh >/usr/bin/sqzlite-cfg.sh
+curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/abcde-cfg.sh >/usr/bin/abcde-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/config.sh >/usr/bin/config.sh
 chmod +x /usr/bin/*.sh
