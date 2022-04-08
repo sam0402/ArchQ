8346cc8777252d9daadec80fb424e0addb65c8c7
sam0402 <sam0402@gmail.com>
2022-04-08 20:31:53 +0800

    Add files via upload

--- config/abcde-cfg.sh
+++ config/abcde-cfg.sh
@@ -29,22 +29,26 @@ options=$(dialog --stdout \
     --form "Modify settings" 0 40 0 \
     "Output directory"  1 1   "${OUTPUTDIR}"    1 18 40 0 \
     "Type (wav/flac)"   2 1   "${OUTPUTTYPE}"   2 18 40 0 \
-    "Read offset"       3 1   "$OFFSET"         3 18 40 0 \
-    "Eject CD (y/n)"    4 1   "$EJECTCD"        4 18 40 0) || exit 1
+    "Read speed"        3 1   "${CDSPEEDVALUE}" 3 18 40 0 \
+    "Read offset"       4 1   "$OFFSET"         4 18 40 0 \
+    "Eject CD (y/n)"    5 1   "$EJECTCD"        5 18 40 0) || exit 1
 clear
 
 OUTPUTDIR=$(echo $options |  awk '//{print $1 }')
 OUTPUTTYPE=$(echo $options |  awk '//{print $2 }')
 OFFSET=$(echo $options |  awk '//{print $3 }')
-EJECTCD=$(echo $options |  awk '//{print $4 }')
+CDSPEEDVALUE=$(echo $options |  awk '//{print $4 }')
+EJECTCD=$(echo $options |  awk '//{print $5 }')
 
 [ -z $OUTPUTDIR ] && echo "Fail! Output directory is null." && exit 1
 [ -z $OUTPUTTYPE ] && echo "Fail! Output type is null." && exit 1
 [ -z $OFFSET ] && echo "Fail! Read offset is null." && exit 1
+[ -z $CDSPEEDVALUE ] && echo "Fail! Read offset is null." && exit 1
 [ -z $EJECTCD ] && echo "Fail! Eject CD is null." && exit 1
 
 OUTPUTDIR=$(echo $OUTPUTDIR | sed 's"/"\\\/"g')
 sed -i 's/^#\?OUTPUTDIR=".*/OUTPUTDIR="'"$OUTPUTDIR"'"/' $config
 sed -i 's/^#\?OUTPUTTYPE=".*/OUTPUTTYPE="'"$OUTPUTTYPE"'"/' $config
 sed -i 's/^#\?OFFSET=".*/OFFSET="'"$OFFSET"'"/' $config
+sed -i 's/^#\?CDSPEEDVALUE=".*/CDSPEEDVALUE="'"$CDSPEEDVALUE"'"/' $config
 sed -i 's/^#\?EJECTCD=.*/EJECTCD='"$EJECTCD"'/' $config
