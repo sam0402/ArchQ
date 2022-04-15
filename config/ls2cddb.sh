59236056f70f6ecb21f601f0688d7a1170f1a691
sam0402 <sam0402@gmail.com>
2022-04-15 11:09:59 +0800

    Add files via upload

--- config/ls2cddb.sh
+++ config/ls2cddb.sh
@@ -4,13 +4,13 @@ cdpath='/mnt/Music/'
 
 cddbfile=$(ls abcde.*/cddbread.0)
 sed -i '/DTITLE/,$d' $cddbfile
-echo "DTITLE=$" >>$cddbfile
+echo "DTITLE=$(echo $1 | sed 's/ - / \/ /')" >>$cddbfile
 echo "DYEAR=" >>$cddbfile
 echo "DGENRE=" >>$cddbfile
 n=-1
 while read line; do
     ((n += 1 ))
-    [[ $(echo $line | grep '.m4a') ]] && echo "TTITLE${n}="$(echo ${line} | sed 's/.m4a//') >>$cddbfile
+    [[ $(echo $line | grep ".${format}") ]] && echo "TTITLE${n}="$(echo ${line} | sed 's/.'"${format}"'//') >>$cddbfile
 done <<< $(ls "$cdpath""$1" | grep $format | cut -d ' ' -f2-)
 echo "EXTD=" >>$cddbfile
 for i in $(seq 0 $n); do
