a9d0052141cd11e498a94941c28088b47fef2405
sam0402 <sam0402@gmail.com>
2022-06-06 08:16:05 +0800

    Update abcde-cfg.sh

--- config/abcde-cfg.sh
+++ config/abcde-cfg.sh
@@ -6,7 +6,8 @@ if [ ! -f $config ]; then
     pacman -Syy --noconfirm
     pacman -S --noconfirm nano cdparanoia glyr imagemagick atomicparsley base-devel
     rm /root/*.pkg.tar.zst
-    wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/kernel/linux-Qrip-5.16.8-2-x86_64.pkg.tar.zst
+    kver=$(pacman -Q | grep linux-Q | grep -v headers | awk 'NR==1{print $2}')
+    wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/kernel/linux-Qrip-${kver}-x86_64.pkg.tar.zst
     wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/abcde-2.9.3-6-any.pkg.tar.zst
     wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/cdparanoia-10.2-9-x86_64.pkg.tar.zst
     wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/cd-discid-1.4-3-x86_64.pkg.tar.zst
