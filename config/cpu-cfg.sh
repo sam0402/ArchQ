f726a64b5ccd6159da4d3cf74858cf3796671889
sam0402 <sam0402@gmail.com>
2022-06-22 23:18:45 +0800

    Update cpu-cfg.sh

--- config/cpu-cfg.sh
+++ config/cpu-cfg.sh
@@ -3,8 +3,7 @@ current=$(cpupower frequency-info | grep "current CPU" | awk '{print $4$5}')
 available=$(cpupower frequency-info | grep 'steps' | cut -d' ' -f7- | sed 's/ G/G/g;s/ M/M/g')
 
 if [[ $current =~ Unableto ]]; then
-    dialog --stdout --title "ArchQ $1" --pause \
-        "\n  Not Available to set CPU frequency." 12 50 10
+    dialog --stdout --title "ArchQ $1" --msgbox "\n Not Available to set CPU frequency." 7 45
 else
     # fan=$(sensors | grep 'RPM' | awk '{print "\n\n"$1,$2,$3,$4}')
     msg="Frequency available:\n$available"
