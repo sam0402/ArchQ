765d6eb3875533f15318f6d6ee6af2a01638b0e0
sam0402 <sam0402@gmail.com>
2022-10-09 20:29:22 +0800

    Add files via upload

--- config/mpd-cfg.sh
+++ config/mpd-cfg.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 config='/etc/mpd.conf'
 
-### Select sound device
-if [ ! $(aplay -L | grep ':') ]; then
-    echo "No Sound Device" ; exit 1
-fi
-
-while read line; do
-    devs+=${line}' 　 '
-done <<< $(aplay -L | grep ':')
-
-device=$(dialog --stdout \
-        --title "ArchQ $1" \
-        --menu "MPD ouput device" 7 0 0 ${devs}) || exit 1
-clear
-sed -i 's/^#\?.* \?\tdevice.*"/\tdevice '"\"$device\""'/' $config
-
-### set music direcroty
-
+### Music direcroty
 mdir=$(grep 'music_directory' $config | awk -F \" '{print $2}' | awk -F \/ '{print $3}')
 
 mdir=$(dialog --stdout \
@@ -26,6 +10,9 @@ mdir=$(dialog --stdout \
     --form "Music directory" 0 30 0 \
     " /mnt/"  1 1 $mdir 1 7 30 0) || exit 1
 clear
+sed -i 's/^#\?music_directory.*"/music_directory "\/mnt\/'"$mdir"'"/' $config
+
+### Volume Control
 grep -q '#[[:space:]]mixer_type "soft' $config && volctl=off || volctl=on
 vol=$(dialog --stdout \
     --title "ArchQ MPD" \
@@ -36,8 +23,40 @@ clear
 [ -n "$vol" ] && sed -i 's/^#.\?mixer_type.*"/\tmixer_type "software"/' $config \
               || sed -i 's/^.\?mixer_type.*"/#\tmixer_type "software"/' $config
 
-sed -i 's/^#\?music_directory.*"/music_directory "\/mnt\/'"$mdir"'"/' $config
+### Audio output
+alsa=on; pulse=off
+if grep -q 'pulse' $config ; then
+    alsa=off; pulse=on
+fi
+output=$(dialog --stdout \
+    --title "ArchQ MPD" \
+    --radiolist "Sound Output" 7 0 0 \
+    S "Sound Card" $alsa \
+    A "Airport" $pulse ) || exit 1
 
-echo $config is setting.
+case $output in
+    S)
+        sed -i 's/type "pulse"/type "alsa"/' $config
+        ### Select sound device
+        if [ ! $(aplay -L | grep ':') ]; then
+            echo "No Sound Device" ; exit 1
+        fi
+
+        while read line; do
+            devs+=${line}' 　 '
+        done <<< $(aplay -L | grep ':')
+
+        device=$(dialog --stdout \
+                --title "ArchQ $1" \
+                --menu "MPD ouput device" 7 0 0 ${devs}) || exit 1
+        clear
+        sed -i 's/^#\?.* \?\tdevice.*"/\tdevice '"\"$device\""'/' $config
+    ;;
+    A)
+        sed -i 's/type "alsa"/type "pulse"/' $config
+        user=$(grep '1000' /etc/passwd | awk -F: '{print $1}')
+        echo "Use command 'pulse_airport' to set Airport output device @$user."
+    ;;
+esac
+
 systemctl restart mpd
-echo MPD is restarted.
