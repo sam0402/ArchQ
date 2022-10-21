ba89d91486bf2a4666c32f1971a5c7cde5b49a07
sam0402 <sam0402@gmail.com>
2022-10-21 15:03:33 +0800

    Update update_scpt.sh

--- config/update_scpt.sh
+++ config/update_scpt.sh
@@ -13,5 +13,9 @@ curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/desktop-cfg.
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/abcde-cfg.sh >/usr/bin/abcde-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/config.sh >/usr/bin/config.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/qboot >/usr/bin/qboot
+[ -f /root/alsa-lib-1.1.9-2-x86_64.pkg.tar.zst ] || wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/alsa-lib-1.1.9-2-x86_64.pkg.tar.zst
+pacman -R --noconfirm alsa-utils
+pacman -U --noconfirm --overwrite '*' /root/alsa-lib-1.1.9-2-x86_64.pkg.tar.zst
+pacman -Sd --noconfirm alsa-utils
 chmod +x /usr/bin/*.sh /usr/bin/qboot
 [ -f '/etc/squeezelite.conf' ] &&  sed -i 's/^OPTIONS="-W .*/#OPTIONS="-W "/' /etc/squeezelite.conf
