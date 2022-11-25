dba69fa5c5fef0712f181bd66097d073eb2b4f9f
sam0402 <sam0402@gmail.com>
2022-11-25 23:52:21 +0800

    Add files via upload

--- config/nfserver-cfg.sh
+++ config/nfserver-cfg.sh
@@ -18,8 +18,9 @@ case $WK in
         ethers=$(ip -o link show | awk '{print $2,$9}' | grep '^en' | sed 's/://')
         ifport=$(echo $ethers | cut -d ' ' -f1)
         if [ $(echo $ethers | wc -w) -gt 2 ]; then
-        ifport=$(dialog --stdout --title "ArchQ $1" \
-            --menu "Select ethernet device" 7 0 0 ${ethers}) || exit 1
+            ifport=$(dialog --stdout --title "ArchQ $1" \
+                    --menu "Select ethernet device" 7 0 0 ${ethers}) || exit 1
+            clear
         fi
         ifnetwk=$(ip route list | grep -v default | grep $ifport | cut -d' ' -f1)
         options=$(dialog --stdout \
