9652e6c211a87ee982abb65259ea5b2087f22b57
sam0402 <sam0402@gmail.com>
2022-12-02 13:40:40 +0800

    Add files via upload

--- config/mpd-cfg.sh
+++ config/mpd-cfg.sh
@@ -2,7 +2,7 @@
 config='/etc/mpd.conf'
 
 ### Music direcroty
-mdir=$(grep 'music_directory' $config | awk -F \" '{print $2}' | cut -d'/' -f3-)
+mdir=$(grep 'music_directory' $config | cut -d'"' -f2 | cut -d'/' -f3-)
 
 mdir=$(dialog --stdout \
     --title "ArchQ MPD" \
@@ -10,18 +10,30 @@ mdir=$(dialog --stdout \
     --form "Music directory" 0 30 0 \
     " /mnt/"  1 1 $mdir 1 7 30 0) || exit 1
 clear
+mdir=$(echo $mdir | sed 's"/"\\\/"g')
 sed -i 's/^#\?music_directory.*"/music_directory "\/mnt\/'"$mdir"'"/' $config
 
 ### Volume Control
-grep -q '#[[:space:]]mixer_type "soft' $config && volctl=off || volctl=on
-vol=$(dialog --stdout \
+v_none='off'; v_soft='off'; v_hard='off'
+case $(grep 'mixer_type' $config | cut -d'"' -f2) in
+    none)
+        v_none=on
+        ;;
+    software)
+        v_soft=on
+        ;;
+    hardware)
+        v_hard=on
+        ;;
+esac
+volume=$(dialog --stdout \
     --title "ArchQ MPD" \
-    --checklist "Volume Control" 7 0 0 \
-    E Enable $volctl ) || exit 1
+    --radiolist "Volume Control" 7 0 0 \
+    none '　' $v_none \
+    software '　' $v_soft \
+    hardware '　' $v_hard) || exit 1
 clear
-
-[ -n "$vol" ] && sed -i 's/^#.\?mixer_type.*"/\tmixer_type "software"/' $config \
-            || sed -i 's/^.\?mixer_type.*"/#\tmixer_type "software"/' $config
+sed -i 's/mixer_type.*"/mixer_type\t"'"$volume"'"/' $config 
 
 ### Audio output
 alsa=on; pulse=off
@@ -36,7 +48,7 @@ output=$(dialog --stdout \
 
 case $output in
     S)
-        sed -i 's/type "pulse"/type "alsa"/' $config
+        sed -i 's/type[[:space:]]*"pulse"/type\t"alsa"/' $config
         ### Select sound device
         if [ ! $(aplay -L | grep ':') ]; then
             echo "No Sound Device" ; exit 1
@@ -50,10 +62,10 @@ case $output in
                 --title "ArchQ $1" \
                 --menu "MPD ouput device" 7 0 0 ${devs}) || exit 1
         clear
-        sed -i 's/^#\?.* \?\tdevice.*"/\tdevice '"\"$device\""'/' $config
+        sed -i 's/^#\?.* \?\tdevice.*"/\tdevice\t'"\"$device\""'/' $config
     ;;
     A)
-        sed -i 's/type "alsa"/type "pulse"/' $config
+        sed -i 's/type[[:space:]]*"alsa"/type\t"pulse"/' $config
         user=$(grep '1000' /etc/passwd | awk -F: '{print $1}')
         echo "Use command 'pulse_airport' to set Airport output device @$user."
     ;;
