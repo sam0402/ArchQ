c9e2aebcd45f658126e0795dc664eb001c5f230a
sam0402 <sam0402@gmail.com>
2022-12-05 23:17:25 +0800

    Add files via upload

--- config/bcache-cfg.sh
+++ config/bcache-cfg.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -e
+# set -e
 WK=$(dialog --stdout --title "ArchQ $1" \
-            --menu "Bcache" 7 0 0 C Creat R Remove) || exit 1
+            --menu "Bcache" 7 0 0 C Create R Remove) || exit 1
 clear
 
 if ! pacman -Q bcache-tools >/dev/null 2>&1; then
     pacman -Sy --noconfirm archlinux-keyring
     pacman -S --noconfirm parted
-    wget -qP /root https://raw.githubusercontent.com/sam0322/ArchQ/main/pkg/bcache-tools-1.1-1-x86_64.pkg.tar.zst
+    wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/bcache-tools-1.1-1-x86_64.pkg.tar.zst
     pacman -U --noconfirm /root/bcache-tools-1.1-1-x86_64.pkg.tar.zst
 fi
 
@@ -17,62 +17,87 @@ case $WK in
         nvmelst=$(lsblk -dplnx size -o name,size | grep "nvme" | tac)
         [[ -z $nvmelst ]] && (echo "No SSD/NVME device." ; exit 1 )
         # Select HDD partiton
-        hdd=$(dialog --stdout --title "Bache creat" --menu "Select HDD" 7 0 0 $hddlst) || exit 1
+        hdd=$(dialog --stdout --title "Bache create" --menu "Select HDD" 7 0 0 $hddlst) || exit 1
         clear
         hddpartlst=$(lsblk -pln -o name,size,fstype $hdd | sed -e '1d;s/\s\+/ /g;s/\s/,/2')
         hddpart=$(dialog --stdout --title "Device $hdd" --menu "Select partition" 7 0 0 $hddpartlst) || exit 1
-        data=$(dialog --stdout --title "Device $hddpart" --menu "Retain data?" 7 0 0 R Retain C Clean)
-        clear
+        bcache=$(lsblk -pln -o name $hddpart | grep bcache | cut -d'/' -f3)
+        if lsblk -pln -o fstype $hddpart | grep -q bcache; then
+            data=B
+        else
+            data=$(dialog --stdout --title "Device $hddpart" --menu "Retain data?" 7 0 0 R Retain C Clean)
+            clear
+        fi
         # Select SSD/NVME partiton
-        nvme=$(dialog --stdout --title "Bache creat" --menu "Select cache SSD" 7 0 0 $nvmelst) || exit 1
+        nvme=$(dialog --stdout --title "Bache create" --menu "Select cache SSD" 7 0 0 $nvmelst) || exit 1
         clear
         nvmepartlst=$(lsblk -pln -o name,size,fstype $nvme | sed -e '1d;s/\s\+/ /g;s/\s/,/2')
         nvmepart=$(dialog --stdout --title "Cache $nvme" --menu "Select partition" 7 0 0 $nvmepartlst) || exit 1
         clear
-        if [ $data = R ]; then
-            # Get sectors range of partition
-            start=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $3 }')
-            starts=$(expr ${start::-1} - 16)s
-            ends=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $4 }')
-            if [ ${hddpart:0-1} -ge 2 ]; then
-                prepartnum=$(expr ${hddpart:0-1} - 1)
-                preends=$(parted $hdd 'unit s' print | grep "^ $prepartnum" | awk -F '[[:space:]]*' '{ print $4 }')
-                [ ${preends::-1} -ge ${starts::-1} ] && ( echo "The partition can not retain data for adding cache!" ; exit 1 )
-            fi
-            # Rebuild parititon
-            # parted $hdd 'unit s' print
-            parted --script $hdd rm ${hddpart:0-1}
-            parted --script $hdd mkpart primary xfs $starts $ends
-        else
-            yes=$(dialog --stdout --title "Bache creat" --yesno "\n  It will clean all data.\nConform to clean $hddpart" 0 0) || exit 1
-            clear
-            [ $yes ] && wipefs -a $hddpart || exit 1
-        fi
+        case $data in
+            R)
+                # Get sectors range of partition
+                start=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $3 }')
+                starts=$(expr ${start::-1} - 16)s
+                ends=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $4 }')
+                if [ ${hddpart:0-1} -eq 1 ]; then
+                    [ $(expr ${start::-1} - 16) -gt 0 ] && work=true
+                else
+                    prepartnum=$(expr ${hddpart:0-1} - 1)
+                    preends=$(parted $hdd 'unit s' print | grep "^ $prepartnum" | awk -F '[[:space:]]*' '{ print $4 }')
+                    [ ${starts::-1} -gt ${preends::-1} ] && work=true
+                fi
+                # Rebuild parititon
+                # parted $hdd 'unit s' print
+                if "$work"; then
+                    parted --script $hdd rm ${hddpart:0-1}
+                    parted --script $hdd mkpart primary xfs $starts $ends
+                    make-bcache -B $hddpart
+                else
+                    echo "Can not create Bcache with retaining data."
+                fi
+            ;;
+            C)
+                yes=$(dialog --stdout --title "Bache create" --yesno "\n  It will clean all data.\nConform to clean $hddpart" 0 0) || exit 1
+                clear
+                [ $yes ] && wipefs -a $hddpart || exit 1
+                make-bcache -B $hddpart
+            ;;
+            B)
+                echo "$bcache is exist."
+            ;;
+        esac
+
         # Build Bcache
-        wipefs -a $nvmepart
-        make-bcache -C $nvmepart
-        make-bcache -B $hddpart
-        echo $(bcache-super-show $nvmepart | grep cset | awk '{print $2}') >/sys/block/bcache0/bcache/attach
-        lsblk
+        lsblk -pln -o fstype $nvmepart | grep -q bcache || (wipefs -a $nvmepart; make-bcache -C $nvmepart)
+        echo $(bcache-super-show $nvmepart | grep cset | awk '{print $2}') >/sys/block/$bcache/bcache/attach
+        [ $? -ne 0 ] && echo -e "\nNeet to do it again or reboot.\n"
+        lsblk $hddpart $nvmepart
         ;;
     R)
         # Remove Bcache
-        bcache=$(lsblk -pln -o name | grep -m1 bcache)
+        bcache=$(lsblk -pln -o name | grep -m1 bcache | cut -d'/' -f3)
         hddpart=$(lsblk -pn -o name | grep -B 1 bcache | grep sd | awk -F '─' '{ print $2}')
         hdd=${hddpart::-1}
         nvme=$(lsblk -pn -o name | grep -B 1 bcache | grep nvme | awk -F '─' '{ print $2}')
         if $(dialog --stdout --title "Bache remove" --yesno "\n  Remove $bcache ?" 7 0); then   
-            umount $bcache
+            umount /dev/$bcache
+            echo /sys/block/$bcache/bcache/detach $nvme
             echo $(bcache-super-show $nvme | grep cset | awk '{print $2}') >/sys/block/$bcache/bcache/detach
             echo 1 >/sys/fs/bcache/`bcache-super-show $nvme | grep cset | awk '{print $2}'`/unregister
             echo 1 >/sys/block/$bcache/bcache/stop
-            # Rebuild partition
-            start=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $3 }')
-            starts=$(expr ${start::-1} + 16)s
-            ends=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $4 }')
-            # parted $hdd 'unit s' print
-            parted --script $hdd rm ${hddpart:0-1}
-            parted --script $hdd mkpart primary xfs $starts $ends
+            if lsblk -pln -o fstype $hddpart | grep -q bcache; then
+                # Rebuild partition
+                start=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $3 }')
+                starts=$(expr ${start::-1} + 16)s
+                ends=$(parted $hdd 'unit s' print | grep "^ ${hddpart:0-1}" | awk -F '[[:space:]]*' '{ print $4 }')
+                # parted $hdd 'unit s' print
+                parted --script $hdd rm ${hddpart:0-1}
+                parted --script $hdd mkpart primary xfs $starts $ends
+            fi
         fi
+        echo -e "\nReboot now [Y/n]? "
+        input=''; read input
+        [[ -z $input || $input = y ]] && reboot
         ;;
 esac
