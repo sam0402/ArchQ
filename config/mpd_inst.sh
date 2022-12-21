eba4c956c9d32af07786329e327b020bb4debfb9
sam0402 <sam0402@gmail.com>
2022-12-21 20:36:07 +0800

    Update mpd_inst.sh

--- config/mpd_inst.sh
+++ config/mpd_inst.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-hostname=$(uname -n)
-pacman -Sy --noconfirm archlinux-keyring
-pacman -S --noconfirm nginx php-sqlite php-gd php-fpm php-intl imagemagick libwmf libjxl mpc which avahi
 wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mpd-light-0.23.9-4-x86_64.pkg.tar.zst
 wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mpd_cdrom-1.0.0-1-any.pkg.tar.zst
-wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/rompr-1.61-1-any.pkg.tar.zst
-pacman -U --noconfirm /root/mpd-light-0.23.9-4-x86_64.pkg.tar.zst /root/mpd_cdrom-1.0.0-1-any.pkg.tar.zst /root/rompr-1.61-1-any.pkg.tar.zst
-
-### Setup RompR
-mkdir -p /etc/nginx/sites-available /etc/nginx/sites-enabled
-sed -i '$i include /etc/nginx/sites-enabled/*;' /etc/nginx/nginx.conf
-curl -sL https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/rompr_nginx >/etc/nginx/sites-available/rompr
-sed -i 's/hostname/'"${hostname,,}"'/' /etc/nginx/sites-available/rompr
-sed -i 's/max_execution_time =.*/max_execution_time = 1800/;s/post_max_size =.*/post_max_size = 256M/;s/upload_max_filesize =.*/upload_max_filesize = 10M/;s/max_file_uploads =.*/max_file_uploads = 200/' /etc/php/php.ini
-sed -i 's/;extension=pdo_sqlite/extension=pdo_sqlite/;s/;extension=gd/extension=gd/;s/;extension=intl/extension=intl/' /etc/php/php.ini
-
-ln -s /etc/nginx/sites-available/rompr /etc/nginx/sites-enabled/rompr
-chmod 644 /etc/nginx/sites-enabled/rompr
+pacman -U --noconfirm /root/mpd-light-0.23.9-4-x86_64.pkg.tar.zst /root/mpd_cdrom-1.0.0-1-any.pkg.tar.zst
+pacman -Sy --noconfirm archlinux-keyring mympd avahi mpc
+systemctl enable --now mympd avahi-daemon
+/usr/bin/update_scpt.sh
+/usr/bin/mpd-cfg.sh
