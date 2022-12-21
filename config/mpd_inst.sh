69bfd4a510db38619ef71b22e4caf8f6b1b61bb5
sam0402 <sam0402@gmail.com>
2022-12-21 20:41:59 +0800

    Update mpd_inst.sh

--- config/mpd_inst.sh
+++ config/mpd_inst.sh
@@ -4,5 +4,3 @@ wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mpd_cdro
 pacman -U --noconfirm /root/mpd-light-0.23.9-4-x86_64.pkg.tar.zst /root/mpd_cdrom-1.0.0-1-any.pkg.tar.zst
 pacman -Sy --noconfirm archlinux-keyring mympd avahi mpc
 systemctl enable --now mympd avahi-daemon
-/usr/bin/update_scpt.sh
-/usr/bin/mpd-cfg.sh
