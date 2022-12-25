fd6737c2c01e40143867fa1fc28e6916f2a8932e
sam0402 <sam0402@gmail.com>
2022-12-25 10:23:16 +0800

    Add files via upload

--- config/mpd-cfg.sh
+++ config/mpd-cfg.sh
@@ -24,7 +24,7 @@ vol_ctrl(){
     sed -i 's/mixer_type.*"/mixer_type\t"'"$volume"'"/' $2
 }
 
-client=$(dialog --stdout --title "ArchQ MPD" --menu "Select MPD client" 7 0 0 R "RompR :6660" M "myMPD :80" N "Ncmpcpp curses") || exit 1
+client=$(dialog --stdout --title "ArchQ MPD" --menu "Select MPD client" 7 0 0 R "RompR :6660" M "myMPD :80" N "Ncmpcpp curses" U "Multi user") || exit 1
 clear
 case $client in
     R)
@@ -41,18 +41,38 @@ case $client in
         systemctl disable --now nginx php-fpm avahi-daemon
         pacman -Q mympd >/dev/null 2>&1 && systemctl disable --now mympd
         ;;
+    U)
+        if [[ ! -d /usr/share/mpd ]]; then
+            curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mpd_config.tar.gz | tar -xz -C /usr/share
+            curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/mpdconf >/usr/bin/mpdconf
+            chmod +x /usr/bin/mpdconf
+        fi
+        users=$(dialog --stdout --title "ArchQ MPD multi" --inputbox "Add users" 0 25 0) || exit 1
+        clear
+        num=$(grep 'mpd' /etc/passwd | wc -l)
+        users=$(( $users + $num - 1 ))
+        for ((i=$num; i <= $users; i++))
+        do
+            useradd -mU "mpd$i"
+            sh -c "echo mpd$i:mpd | chpasswd"
+            mkdir -p /home/mpd$i/.config
+            cp -a /usr/share/mpd /home/mpd$i/.config
+            chown -R mpd$i: /home/mpd$i/.config
+            port=$(( $(id -u mpd$i) + 5600 ))
+            sed -i 's/port.*"/port\t"'"$port"'"/' /home/mpd$i/.config/mpd/mpd.conf
+            sport=$(( $(id -u mpd$i) + 7000 ))
+            sed -i 's/port.*"/port\t"'"$sport"'"/' /home/mpd$i/.config/mpd/httpd.out
+            ln -s /usr/lib/systemd/user/mpd.service /usr/lib/systemd/user/mpd$i.service
+            cp /usr/lib/systemd/user/mpd.socket /usr/lib/systemd/user/mpd$i.socket
+            sed -i 's/ListenStream=6600/ListenStream='"$port"'/' /usr/lib/systemd/user/mpd$i.socket
+            systemctl enable --now avahi-daemon
+            systemctl --user -M mpd$i@ enable mpd$i.socket
+            echo "User mpd$i's control port: $port, stream port: $sport"
+        done
+        echo "Use command 'mpdcfg mpd[$num-$users]' to configure."
+        exit 1
+        ;;
 esac
-### Music direcroty 
-mdir=$(grep 'music_directory' $config | cut -d'"' -f2 | cut -d'/' -f3-)
-
-mdir=$(dialog --stdout \
-    --title "ArchQ MPD" \
-    --ok-label "Ok" \
-    --form "Music directory" 0 30 0 \
-    " /mnt/"  1 1 $mdir 1 7 30 0) || exit 1
-clear
-mdir=$(echo $mdir | sed 's"/"\\\/"g')
-sed -i 's/^#\?music_directory.*"/music_directory "\/mnt\/'"$mdir"'"/' $config
 
 # ### Select sound device
 if [ ! $(aplay -L | grep ':') ]; then
@@ -80,7 +100,7 @@ cat $config | grep httpd | grep -q '#' || h0=on
 output=$(dialog --stdout --title "ArchQ MPD" \
         --checklist "Include output" 7 0 0 \
         A Airplay $p0 H Httpd $h0 ) || exit 1
-
+clear
 [[ $output =~ A ]] && p1=on
 [[ $output =~ H ]] && h1=on
 
@@ -104,9 +124,22 @@ if [[ $h1 == on ]]; then
         --radiolist "Httpd stream encoder" 7 0 0 \
         flac '　' $http_flac \
         wave '　' $http_wave) || exit 1
+    clear
     sed -i 's/encoder.*"/encoder\t"'"$encoder"'"/' $ht_conf
 else   
     sed -i 's/^include_optional "mpd.d\/httpd.out"/#include_optional "mpd.d\/httpd.out"/' $config
 fi
+
+### Music direcroty 
+mdir=$(grep 'music_directory' $config | cut -d'"' -f2 | cut -d'/' -f3-)
+
+mdir=$(dialog --stdout \
+    --title "ArchQ MPD" \
+    --ok-label "Ok" \
+    --form "Music directory" 0 30 0 \
+    " /mnt/"  1 1 $mdir 1 7 30 0) || exit 1
+clear
+mdir=$(echo $mdir | sed 's"/"\\\/"g')
+sed -i 's/^#\?music_directory.*"/music_directory "\/mnt\/'"$mdir"'"/' $config
 
 systemctl restart mpd
