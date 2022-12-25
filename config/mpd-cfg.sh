a3635294a748699d510c2b2b87d953b893c05c46
sam0402 <sam0402@gmail.com>
2022-12-25 12:44:46 +0800

    Update mpd-cfg.sh

--- config/mpd-cfg.sh
+++ config/mpd-cfg.sh
@@ -113,7 +113,7 @@ if [[ $p1 == on ]]; then
     echo "Use command 'pulse_airport' to set Airport output device @$user."
 else
     sed -i 's/^include_optional "mpd.d\/pulse.out"/#include_optional "mpd.d\/pulse.out"/' $config
-    systemctl --user -M $user@ disable pipewire pipewire-pulse sinkdef
+  #  systemctl --user -M $user@ disable pipewire pipewire-pulse sinkdef
 fi
 
 if [[ $h1 == on ]]; then
