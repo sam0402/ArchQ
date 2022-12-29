ed3e44f065504866d4302bd08c3fc154658282ef
sam0402 <sam0402@gmail.com>
2022-12-29 18:41:33 +0800

    Update mpd_inst.sh

--- config/mpd_inst.sh
+++ config/mpd_inst.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mpd-light-0.23.9-4-x86_64.pkg.tar.zst
 wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mpd_cdrom-1.0.0-1-any.pkg.tar.zst
-pacman -U --noconfirm /root/mpd-light-0.23.9-4-x86_64.pkg.tar.zst /root/mpd_cdrom-1.0.0-1-any.pkg.tar.zst
-pacman -Sy --noconfirm archlinux-keyring mympd avahi mpc
-systemctl enable --now mympd avahi-daemon
+wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/owntone-28.5-1-x86_64.pkg.tar.zst
+pacman -Sy --noconfirm archlinux-keyring
+pacman -U --noconfirm /root/*.pkg.tar.zst
+pacman -Sy --noconfirm mympd avahi mpc
+systemctl enable --now mympd
