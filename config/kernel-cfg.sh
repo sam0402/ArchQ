36e456a34653475a0c7056706a9d25e9674bdef4
sam0402 <sam0402@gmail.com>
2023-04-01 00:18:12 +0800

    Add files via upload

--- config/kernel-cfg.sh
+++ config/kernel-cfg.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 config='/etc/fstab'
+grub_cfg='/boot/grub/grub.cfg'
 WK=$(dialog --stdout --title "ArchQ $1" \
-            --menu "Select command" 7 0 0 B "Boot" I "Install" R "Remove" F "Frequency") || exit 1
-clear
+            --menu "Select command" 7 0 0 B "Boot" I "Install" R "Remove" F "Frequency") || exit 1; clear
+part_boot=$(lsblk -pln -o name,parttypename | grep EFI | awk 'NR==1 {print $1}')
 case $WK in
     I)
         exec='dialog --stdout --title "ArchQ '$1'" --menu "Select to install" 7 0 0 '
@@ -21,7 +22,7 @@ case $WK in
             [ ! -f "/root/linux-${ver}-${kver}-x86_64.pkg.tar.zst" ] && wget -P /root https://raw.githubusercontent.com/sam0402/ArchQ/main/kernel/linux-${ver}-${kver}-x86_64.pkg.tar.zst
             pacman -U --noconfirm /root/linux-${ver}-${kver}-x86_64.pkg.tar.zst
         fi
-        grub-mkconfig -o /boot/grub/grub.cfg
+        grub-mkconfig -o $grub_cfg
         ;;
     R)
         while read line; do
@@ -33,26 +34,29 @@ case $WK in
         clear
         echo Rmove Kernel Q1xx ...
         pacman -R ${options}
-        grub-mkconfig -o /boot/grub/grub.cfg
+        grub-mkconfig -o $grub_cfg
         ;;
     B)
+        mount "$part_boot" /mnt
+        os-prober
+        umount /mnt
+        grub-mkconfig -o $grub_cfg
         grub_def='/etc/default/grub'
         if [ -n "$(grep '#GRUB_DISABLE_SUBMENU' $grub_def)" ]; then 
             sed -i 's/^#\?GRUB_DISABLE_SUBMENU=.*$/GRUB_DISABLE_SUBMENU=y/' $grub_def
-            grub-mkconfig -o /boot/grub/grub.cfg
+            grub-mkconfig -o $grub_cfg
         fi
-        n=0; menu=''
+        n=0
+        bootlist='dialog --stdout --title "ArchQ $1" --menu "Select default boot" 7 0 0 '
         while read line; do
-            menu+=${n}' '${line}' '
-            n=`expr $n + 2`
-        done <<< $(grep 'with' /boot/grub/grub.cfg | grep -v 'fallback' | cut -d "'" -f2 | cut -d ' ' -f5)
-        options=$(dialog --stdout \
-                --title "ArchQ $1" \
-                --menu "Select default boot" 7 0 0 $menu S "Save Default") || exit 1
-        clear
+            echo $line | grep 'fallback' || bootlist+=$n' '\"$line\"' '
+            n=`expr $n + 1`
+        done <<< $(grep 'menuentry ' $grub_cfg | cut -d "'" -f2 | sed '$d;s/Arch Linux, with Linux //;s/ initramfs//'|cut -d' ' -f1-2)
+        options=$(eval $bootlist) || exit 1; clear
+
         [ $options = 'S' ] && sed -e 's/^#\?GRUB_SAVEDEFAULT=.*$/GRUB_SAVEDEFAULT=true/' $grub_def
         sed -i 's/^#\?GRUB_DEFAULT=.*$/GRUB_DEFAULT='"$options"'/' $grub_def
-        grub-mkconfig -o /boot/grub/grub.cfg
+        grub-mkconfig -o $grub_cfg
         ;;
     F)
         num=$(getconf _NPROCESSORS_ONLN)
