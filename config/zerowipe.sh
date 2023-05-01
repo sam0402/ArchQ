41d69363b9a52696e999a25f99a172dd8a167b90
sam0402 <sam0402@gmail.com>
2023-05-01 11:38:06 +0800

    Add files via upload

--- config/zerowipe.sh
+++ config/zerowipe.sh
@@ -5,10 +5,10 @@ WK=$(dialog --stdout --title "ArchQ $1" \
 clear
 case $WK in
     W)
-        if ! pacman -Q scrub >/dev/null 2>&1; then
-            wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/scrub-2.6.1-1-x86_64.pkg.tar.zst
-            pacman -U --noconfirm /root/scrub-2.6.1-1-x86_64.pkg.tar.zst
-        fi
+        # if ! pacman -Q scrub >/dev/null 2>&1; then
+        #     wget -qP /root https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/scrub-2.6.1-1-x86_64.pkg.tar.zst
+        #     pacman -U --noconfirm /root/scrub-2.6.1-1-x86_64.pkg.tar.zst
+        # fi
         devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | grep sd | tac)
         device=$(dialog --stdout --title "Wipe disk" --menu "Select HDD device" 7 0 0 $devicelist) || exit 1; clear
         partitionlist=$(lsblk -pln -o name,size $device | sed -e '1d;s/\s\+/ /g')
