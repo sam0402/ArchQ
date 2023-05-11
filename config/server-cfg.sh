b8fb54a815fd13838ad068cbbd194f6611131a26
sam0402 <sam0402@gmail.com>
2023-05-11 16:52:34 +0800

    Add files via upload

--- config/server-cfg.sh
+++ config/server-cfg.sh
@@ -87,6 +87,7 @@ EOF
     H)
         ver=5.0.0-2avx2
         wget -O - https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/hqplayerd-lib.tar.gz | tar zxf - -C /tmp
+        wget -P /tmp/hqplayerd-lib https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/gtk3-1%3A3.24.37-1-x86_64.pkg.tar.zst
         wget -O - https://www.signalyst.eu/bins/hqplayerd/jammy/hqplayerd_"$ver"_amd64.deb | bsdtar xf - -C /tmp
         pacman -U --noconfirm /tmp/hqplayerd-lib/*.pkg.tar.zst
         bsdtar xf /tmp/data.tar.zst -C /
