7fd734d0de19b5b2338a9ea60e332ae2acb5da8b
sam0402 <sam0402@gmail.com>
2023-05-16 01:03:46 +0800

    Add files via upload

--- config/desktop-cfg.sh
+++ config/desktop-cfg.sh
@@ -6,7 +6,7 @@ desktop=$(dialog --stdout --title "ArchQ" --menu "Desktop & VNC :5901" 7 0 0 D L
 clear
 
 cat /etc/locale.conf >/home/$user/.xinitrc
-if [[ ! $(pacman -Q lxdm | cut -f1) ]]; then
+if ! pacman -Q lxdm >/dev/null 2>&1; then
     pacman -Sy archlinux-keyring
     pacman -Scc --noconfirm
     pacman -Syy --noconfirm
@@ -24,31 +24,28 @@ if [[ ! $(pacman -Q lxdm | cut -f1) ]]; then
 fi
 case $desktop in
     D)
-        if [[ ! $(pacman -Q lxsession | cut -f1) ]]; then
+        if ! pacman -Q lxsession >/dev/null 2>&1; then
             pacman -S --noconfirm lxde lxpanel
             pacman -R --noconfirm lxmusic
         fi
         sed -i 's;^# session=/usr/bin/startlx??;session=/usr/bin/startlxde;g' /etc/lxdm/lxdm.conf
         sed -i 's;^session=.*;session=LXDE;g' /home/$user/.vnc/config
         ln -sf /usr/bin/lxterminal /usr/bin/xterm
-        systemctl enable lxdm vncserver@:1.service
-        systemctl restart lxdm vncserver@:1.service
+        systemctl enable --now lxdm vncserver@:1.service
         echo "Enable LXDE & VNC ..."
         ;;
     Q)
-        if [[ ! $(pacman -Q lxqt-session | cut -f1) ]]; then
+        if ! pacman -Q lxqt-session >/dev/null 2>&1; then
             pacman -S --noconfirm lxqt xdg-utils breeze-icons fcitx5-qt fcitx5-chewing fcitx5-mozc
         fi
         sed -i 's;^# session=/usr/bin/startlx??;session=/usr/bin/startlxqt;g' /etc/lxdm/lxdm.conf
         sed -i 's;^session=.*;session=lxqt;g' /home/$user/.vnc/config
         ln -sf /usr/bin/qterminal /usr/bin/xterm
-        systemctl enable lxdm vncserver@:1.service
-        systemctl restart lxdm vncserver@:1.service
+        systemctl enable --now lxdm vncserver@:1.service
         echo "LXQt & VNC is enabled."
         ;;
     V)
-        systemctl disable lxdm
-        systemctl stop lxdm
+        systemctl disable --now lxdm
         ;;
     N)
         systemctl disable lxdm vncserver@:1.service
@@ -76,12 +73,11 @@ if [ ! -e "/home/$user/.vnc/passwd" ]; then
     clear
     [[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
 
-    cp /usr/lib/systemd/system/vncserver\@.service /etc/systemd/system/vncserver@:1.service
+    ln /usr/lib/systemd/system/vncserver\@.service /etc/systemd/system/vncserver@:1.service
     echo ":1=$user" >>/etc/tigervnc/vncserver.users
     echo $password | vncpasswd -f >/home/$user/.vnc/passwd
     chmod 600 /home/$user/.vnc/passwd
     chown -R ${user}: /home/$user/.vnc
-    systemctl enable vncserver@:1.service
-    systemctl start vncserver@:1.service
+    systemctl enable --now vncserver@:1.service
     echo "VNC password setuped."
 fi
