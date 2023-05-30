682726c0864f1cf46d32c9f516bc070d8ce7ff87
sam0402 <sam0402@gmail.com>
2023-05-30 09:38:32 +0800

    Update desktop-cfg.sh

--- config/desktop-cfg.sh
+++ config/desktop-cfg.sh
@@ -11,6 +11,7 @@ case $desktop in
         if ! pacman -Q lxsession >/dev/null 2>&1; then
             pacman -Sy --noconfirm lxdm noto-fonts-cjk tigervnc midori cantata fcitx5-im fcitx5-configtool falkon lxde lxpanel
             pacman -R --noconfirm lxmusic
+            sed -i 's/# autologin=.*/autologin='"$user"'/;s/# timeout=.*/timeout=0/' /etc/lxdm/lxdm.conf
         fi
         sed -i 's;^session=.*;session=/usr/bin/startlxde;g' /etc/lxdm/lxdm.conf
         sed -i 's;^session=.*;session=LXDE;g' /home/$user/.vnc/config
@@ -23,6 +24,7 @@ case $desktop in
             pacman -Sy --noconfirm lxdm noto-fonts-cjk tigervnc midori cantata fcitx5-im fcitx5-configtool falkon \
                 lxqt xdg-utils breeze-icons fcitx5-qt fcitx5-chewing fcitx5-mozc
             pacman -R --noconfirm lxqt-powermanagement
+            sed -i 's/# autologin=.*/autologin='"$user"'/;s/# timeout=.*/timeout=0/' /etc/lxdm/lxdm.conf
         fi
         sed -i 's;^session=.*;session=/usr/bin/startlxqt;g' /etc/lxdm/lxdm.conf
         sed -i 's;^session=.*;session=lxqt;g' /home/$user/.vnc/config
