185befd14656b4b140befc8e0de96d4822d51b0b
sam0402 <sam0402@gmail.com>
2023-06-12 08:26:21 +0800

    Update update_scpt.sh

--- config/update_scpt.sh
+++ config/update_scpt.sh
@@ -14,6 +14,7 @@ curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/sqzlite-cfg.
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/abcde-cfg.sh >/usr/bin/abcde-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/bcache-cfg.sh >/usr/bin/bcache-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/datacache-cfg.sh >/usr/bin/datacache-cfg.sh
+curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/hddpower-cfg.sh >/usr/bin/hddpower-cfg.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/zerowipe.sh >/usr/bin/zerowipe.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/config.sh >/usr/bin/config.sh
 curl -L https://raw.githubusercontent.com/sam0402/ArchQ/main/config/sw >/usr/bin/sw
