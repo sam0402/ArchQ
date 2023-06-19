8f6f671d1b42078945b2dddd96a588e4268a0ace
sam0402 <sam0402@gmail.com>
2023-06-19 23:23:42 +0800

    Add files via upload

--- config/hddpower-cfg.sh
+++ config/hddpower-cfg.sh
@@ -3,10 +3,10 @@ file='/usr/lib/systemd/system-shutdown/hdd-poweroff'
 arrDev=()
 MENU=''; i=0
 while read line; do
-    arrDev+=$line
-    MENU=${MENU}' '$i' '$(echo $line | cut -d '_' -f1)
+    arrDev+=($line)
+    MENU=${MENU}' '$i' '$(echo $line | cut -d '_' -f1-2)
+    ((++i))
     grep -q "$line" "$file" && MENU=${MENU}' on ' || MENU=${MENU}' off ' 
-    i++
 done <<< $(ls /dev/disk/by-id/usb-* | grep -v 'part' | cut -d '/' -f5)
 
 options=$(dialog --stdout \
@@ -14,9 +14,8 @@ options=$(dialog --stdout \
         --checklist "Select device" 7 0 0 $MENU) || exit 1; clear
 
 echo '#!/bin/sh' >$file
-for ((i=0; i < ${#arrDev[@]}; i++))
-do
-    echo $options | grep -q $i && \
-        echo '[ "$1" = "poweroff" ] && /usr/bin/hdparm -f -F -Y /dev/disk/by-id/'${arrDev[$options]} >>$file
+for i in $options
+do  
+    echo '[ "$1" = "poweroff" ] && /usr/bin/hdparm -f -F -Y /dev/disk/by-id/'${arrDev[$i]} >>$file
 done
 chmod +x $file
