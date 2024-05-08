51378cbb4a2f859648d67d15a9907273a0bf4ec1
sam0402 <sam0402@gmail.com>
2024-05-09 00:01:23 +0800

    Update usb-storage-mount.sh

--- pkg/usb-storage-mount.sh
+++ pkg/usb-storage-mount.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 /usr/bin/systemd-mount --no-block --automount=yes --collect \
-UUID=$1 `cat /etc/fstab | grep -v '#' | grep $1 | cut -d' ' -f 2`
+UUID=$1 `cat /etc/fstab | grep $1 | cut -d' ' -f 2`
