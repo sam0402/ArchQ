dd62e38b3eba7acc9275bb7c5272b15723a2096b
sam0402 <sam0402@gmail.com>
2024-11-08 11:15:05 +0800

    Update config.sh

--- config/config.sh
+++ config/config.sh
@@ -8,6 +8,7 @@ ipaddr=$(ip -o addr | grep en | awk 'NR == 1 {print $4}')
 MENU=''
 [ $gitupd -gt $num ] && MENU+='U Update '
 pacman -Q mpd-light >/dev/null 2>&1 && MENU+='D MPD '
+pacman -Q mpd-stream >/dev/null 2>&1 && MENU+='D MPD ' 
 pacman -Q mpd-ffmpeg >/dev/null 2>&1 && MENU+='D MPD ' 
 pacman -Q squeezelite >/dev/null 2>&1 && MENU+='S Squeezelite '
 pacman -Q shairport-sync >/dev/null 2>&1 && MENU+='A Airplay '
