944dc63618bee5f827ff94d3648a080c92eec026
sam0402 <sam0402@gmail.com>
2025-03-27 17:48:50 +0800

    Add files via upload

--- config/server-cfg.sh
+++ config/server-cfg.sh
@@ -149,7 +149,7 @@ EOF
             wget -P /tmp https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mpd-${MPD}-${mpdver}-x86_64.pkg.tar.zst
             pacman -R --noconfirm $(pacman -Q mpd | awk '{print $1}')
             pacman -U --noconfirm /tmp/mpd-${MPD}-${mpdver}-x86_64.pkg.tar.zst
-            sed -i 's|ExecStart=|ExecStart=/usr/bin/pagecache-management.sh |' /usr/lib/systemd/system/mpd.service
+            sed -i 's|ExecStart=|ExecStart=/usr/bin/pagecache-management.sh |;/LimitRTPRIO/iNice=-20' /usr/lib/systemd/system/mpd.service
         fi
         ### Start mpd.. etc. service
         sed -i 's/'"$isocpu"'//' /etc/default/grub
