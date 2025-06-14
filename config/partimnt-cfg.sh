13f3441f37592ef1460bf032b7b33705e48e4b75
sam0402 <sam0402@gmail.com>
2025-06-14 21:38:01 +0800

    Add files via upload

--- config/partimnt-cfg.sh
+++ config/partimnt-cfg.sh
@@ -9,7 +9,7 @@ case $WK in
         devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
         device=$(dialog --stdout --title "Mount partition" --menu "Select device" 7 0 0 $devicelist) || exit 1; clear
         partitionlist=$(lsblk -pln -o name,size,fstype $device | sed -e '1d;s/\s\+/ /g;s/\s/,/2')
-        [ "$partitionlist" = "" ] && (echo "No partitions are found on the $device"; exit 0)
+        [ "$partitionlist" = "" ] && (echo "There are no partitions on $device."; exit 0)
         partition=$(dialog --stdout --title "Device $device" --menu "Select partition" 7 0 0 $partitionlist) || exit 1; clear
         partdata=$(lsblk -pln -o name,fstype,uuid $partition)
         PT=$(echo $partdata | cut -d ' ' -f 1)
@@ -36,11 +36,11 @@ case $WK in
         OP=$(echo $options |  awk '//{print $2 }')
         [ $FS = xfs ] && OP+=',attr2,inode64,logbufs=8,logbsize=32k,noquota'
         [ $FS = f2fs ] && OP+=',background_gc=on,no_heap,inline_xattr,inline_data,inline_dentry,flush_merge,extent_cache,mode=adaptive,active_logs=6,alloc_mode=reuse,checkpoint_merge,fsync_mode=posix,discard_unit=block,memory=normal'
-        [ -z $OP ] && echo "Fail! Mount point is null." && exit 1
+        [ -z $OP ] && echo "Error: Missing mount point." && exit 1
 
     # mount for root || user
         mntuser=$(dialog --stdout --title "Mount point /mnt/$MP" \
-            --radiolist "Set permission to" 7 0 0 \
+            --radiolist "Set permissions for" 7 0 0 \
             $user '　' on \
              root '　' off ) || exit 1; clear
 
@@ -49,7 +49,7 @@ case $WK in
 
     # for USB storage
         usb=$(dialog --stdout --title "Mount point /mnt/$MP" \
-            --radiolist "USB storage auto mount" 7 0 0 \
+            --radiolist "Auto-mount USB storage" 7 0 0 \
             No '　' on \
             Yes '　' off ) || exit 1; clear
 
@@ -77,11 +77,11 @@ case $WK in
 
         options=$(dialog --stdout \
                 --title "Eject partition" \
-                --menu "Select to delete" 7 0 0 $MENU) || exit 1; clear
+                --menu Select partition to delete 7 0 0 $MENU) || exit 1; clear
         MP=$(echo $options | cut -d '/' -f 3)
         umount /mnt/$MP
         sed -i '/\/mnt\/'"$MP"'/d' $config
         systemctl daemon-reload
-        echo Eject /mnt/$MP and delete mount point.
+        echo "Unmount /mnt/$MP and clean up the mount point."
         ;;
 esac
