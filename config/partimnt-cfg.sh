9494c008ad8344a4886cdddd2bc5ab922c893f01
sam0402 <sam0402@gmail.com>
2025-07-17 07:33:47 +0800

    Update partimnt-cfg.sh

--- config/partimnt-cfg.sh
+++ config/partimnt-cfg.sh
@@ -35,7 +35,7 @@ case $WK in
         MP=$(echo $options |  awk '//{print $1 }')
         OP=$(echo $options |  awk '//{print $2 }')
         [ $FS = xfs ] && OP+=',attr2,inode64,logbufs=8,logbsize=32k,noquota'
-        [ $FS = f2fs ] && OP+=',background_gc=on,no_heap,inline_xattr,inline_data,inline_dentry,flush_merge,extent_cache,mode=adaptive,active_logs=6,alloc_mode=reuse,checkpoint_merge,fsync_mode=posix,discard_unit=block,memory=normal'
+        [ $FS = f2fs ] && OP+=',background_gc=on,nogc_merge,discard,discard_unit=block,inline_xattr,inline_data,inline_dentry,flush_merge,extent_cache,mode=adaptive,active_logs=6,alloc_mode=reuse,checkpoint_merge,fsync_mode=posix,memory=normal,errors=continue'
         [ -z $OP ] && echo "Error: Missing mount point." && exit 1
 
     # mount for root || user
