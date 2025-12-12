2a3589e70f558baf5395e5cb55f619853b1eb6b7
sam0402 <sam0402@gmail.com>
2025-12-12 11:38:04 +0800

    Add files via upload

--- config/sync_backup-cfg.sh
+++ config/sync_backup-cfg.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
+
 while read line; do
     list+=${line}' 　 '
 done <<< $(ls /mnt)
 
-SOURCE=$(dialog --stdout \
-        --title "Synchronization Backup $1" \
-        --menu "Source (/mnt)" 7 0 0 ${list}) || exit 1; clear
+WK=$(dialog --stdout \
+        --title "ArchQ $1" \
+        --menu "Backup/Restore Music" 7 0 0 Backup '　' Restore '　') || exit 1; clear
+case $WK in
+    Backup)
+        reinfo=target
+        SOURCE=$(dialog --stdout \
+                --title "Synchronization Backup" \
+                --menu "Music files from (/mnt)" 7 0 0 ${list}) || exit 1; clear
+        ;;
+
+    Restore)
+        reinfo=source
+        TARGET=$(dialog --stdout \
+                --title "Restore music files" \
+                --menu "To music directory (/mnt)" 7 0 0 ${list}) || exit 1; clear
+        ;;
+esac
 
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
-device=$(dialog --stdout --title "Synchronization Backup $1" --menu "Select target disk" 7 0 0 $devicelist) || exit 1; clear
+device=$(dialog --stdout --title "Synchronization $WK $1" --menu "Select $reinfo disk" 7 0 0 $devicelist) || exit 1; clear
 partitionlist=$(lsblk -pln -o name,size,fstype $device | sed -e '1d;s/\s\+/ /g;s/\s/,/2')
-partition=$(dialog --stdout --title "Synchronization Backup $device" --menu "Select partition" 7 0 0 $partitionlist) || exit 1; clear
+partition=$(dialog --stdout --title "Synchronization $WK" --menu "Select $reinfo partition" 7 0 0 $partitionlist) || exit 1; clear
 partdata=$(lsblk -pln -o name,fstype $partition)
 
-options=$(dialog --stdout --title "Synchronization Backup $1" --checklist "" 7 0 0 \
+options=$(dialog --stdout --title "Synchronization $WK $1" --checklist "" 7 0 0 \
         D "Delete files don't exist in the source" on \
-        U "Umount partition after finished" on) || exit 1; clear
+        U "Umount partition(/mnt/muz_bk) after finished" on) || exit 1; clear
 [[ $options =~ D ]] && DEL="--delete"
 [[ $options =~ D ]] && delmsg=",\nand deletes files in the destination if they don't exist in the source"
 
@@ -34,10 +50,22 @@ fi
 [ $FS = xfs ] && OP+=',attr2,inode64,logbufs=8,logbsize=32k,noquota'
 [ $FS = f2fs ] && OP+=',background_gc=on,no_heap,inline_xattr,inline_data,inline_dentry,flush_merge,extent_cache,mode=adaptive,active_logs=6,alloc_mode=reuse,checkpoint_merge,fsync_mode=posix,discard_unit=block,memory=normal'
 
-yes=$(dialog --stdout --title "Synchronization Backup $1" \
-        --yesno "This will backup files from\n/mnt/$SOURCE to $partition (/mnt/music_bk)." 0 0) || exit 1; clear
- 
-echo -e "Synchronization backup /mnt/$SOURCE to ${partition}${delmsg}."
-mount -t $FS -m -o $OP $partition /mnt/music_bk
-nocache rsync -avh $DEL --progress /mnt/$SOURCE/. /mnt/music_bk/.
-[[ $options =~ D ]] && umount /mnt/music_bk
+case $WK in
+    Backup)
+        yes=$(dialog --stdout --title "Synchronization ${WK^^} $1" \
+                --colors --yesno "Backup files from\n/mnt/$SOURCE to \Z1$partition\Zn." 0 0) || exit 1; clear
+        echo -e "Synchronization backup /mnt/$SOURCE to ${partition}${delmsg}."
+        mount -t $FS -m -o $OP $partition /mnt/muz_bk
+        nocache rsync -avh $DEL --progress /mnt/$SOURCE/. /mnt/muz_bk/.
+         ;;
+
+    Restore)
+        yes=$(dialog --stdout --title "Synchronization ${WK^^} $1" \
+                --colors --yesno "Restore files from\n$partition to \Z1/mnt/$TARGET\Zn." 0 0) || exit 1; clear
+        echo -e "Restore ${partition}${delmsg} to /mnt/$SOURCE."
+        mount -t $FS -m -o $OP $partition /mnt/muz_bk
+        nocache rsync -avh $DEL --progress /mnt/muz_bk/. /mnt/$TARGET/.
+        ;;
+esac
+
+[[ $options =~ D ]] && umount /mnt/muz_bk
