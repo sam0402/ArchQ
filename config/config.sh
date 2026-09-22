69bf7034dacfda6679a93120260978bebfb818b4
sam0402 <sam0402@gmail.com>
2026-09-22 23:35:40 +0800

    Add files via upload

--- config/config.sh
+++ config/config.sh
@@ -10,9 +10,9 @@ MENU=''
 pacman -Q mpd >/dev/null 2>&1 && MENU+='D MPD '
 pacman -Q squeezelite >/dev/null 2>&1 && MENU+='S Squeezelite '
 pacman -Q shairport-sync >/dev/null 2>&1 && MENU+='A Airplay '
-
+grep -q HugePages /proc/meminfo && MENU+='G HugePages ' || MENU+='G DataCache'
 if [ -f /root/.advence ]; then
-   MENU2='R "abCDe ripper" P Player O Server I "Service mode" J "Backup/Restore" G "Data cache" C "CPU frequency" Z "Format/0 Wipe" N "NFS mount" B "SMB/CIFS mount" E Network V "NFS Server" Y Bcache T Timezone '
+   MENU2='R "abCDe ripper" P Player O Server I "Service mode" J "Backup/Restore" C "CPU frequency" Z "Format/0 Wipe" N "NFS mount" B "SMB/CIFS mount" E Network V "NFS Server" Y Bcache T Timezone '
 	find /dev/disk/by-id/usb-* | grep -q 'usb' && MENU2+='W "HDD Poweroff" '
 	if pacman -Q ffmpeg >/dev/null 2>&1; then
         { [[ $(pacman -Q ffmpeg) != 'ffmpeg 2:5.1.2-12' ]] || [[ -d '/opt/RoonServer' ]]; } && MENU2+='F FFmpeg '
@@ -60,7 +60,8 @@ case $options in
         fi
         ;;
     G)
-        /usr/bin/datacache-cfg.sh $KVER
+        grep -q HugePages /proc/meminfo && /usr/bin/datacache-cfg.sh $KVER hugepages || /usr/bin/datacache-cfg.sh $KVER datacache
+        
         ;;
     H)
         # pacman -Sy --noconfirm archlinux-keyring
--- config/datacache-cfg.sh
+++ config/datacache-cfg.sh
@@ -2,7 +2,6 @@
 serpath='/usr/lib/systemd/system/'
 service=("mpd" "lyrionmusicserver" "squeezelite" "shairport-sync" "owntone" "hqplayerd" "networkaudio")
 nickname=("MPD" "LMS" "Squeezelite" "Airplay" "OwnTone" "HQPlayerEmbedded" "NAA")
-
 arrList=(); arrService=()
 for ((i=0; i < ${#service[@]}; i++))
 do
@@ -13,20 +12,52 @@ do
 done
 
 menu='';
-for ((i=0; i < ${#arrList[@]}; i++))
-do
-    menu+=$i' '${arrList[$i]}
-    grep -q pagecache-management "${serpath}${arrService[$i]}.service" && menu+=' on ' || menu+=' off '
-done
+case $2 in
+    datacache)
+        for ((i=0; i < ${#arrList[@]}; i++))
+        do
+            menu+=$i' '${arrList[$i]}
+            grep -q pagecache-management "${serpath}${arrService[$i]}.service" && menu+=' on ' || menu+=' off '
+        done
 
-options=$(dialog --stdout --title "ArchQ $1" --checklist "Data cache OFF" 7 0 0 ${menu}) || exit 1; clear
-for ((i=0; i < ${#arrList[@]}; i++))
-do
-    if ( echo $options | grep -q $i ); then
-        grep -q pagecache-management "${serpath}${arrService[$i]}.service" || \
-        sed -i 's|ExecStart=|ExecStart=/usr/bin/pagecache-management.sh |' "${serpath}${arrService[$i]}.service"
-    else
-        sed -i 's|ExecStart=/usr/bin/pagecache-management.sh |ExecStart=|' "${serpath}${arrService[$i]}.service"
-    fi
-done
+        options=$(dialog --stdout --title "ArchQ $1" --checklist "Data cache OFF" 7 0 0 ${menu}) || exit 1; clear
+        for ((i=0; i < ${#arrList[@]}; i++))
+        do
+            if ( echo $options | grep -q $i ); then
+                grep -q pagecache-management "${serpath}${arrService[$i]}.service" || \
+                sed -i 's|ExecStart=|ExecStart=/usr/bin/pagecache-management.sh |' "${serpath}${arrService[$i]}.service"
+                sed -i '/Environment=/d' "${serpath}${arrService[$i]}.service"
+            else
+                sed -i 's|ExecStart=/usr/bin/pagecache-management.sh |ExecStart=|' "${serpath}${arrService[$i]}.service"
+            fi
+        done
+    ;;
+    hugepages)
+        if ! pacman -Q mimalloc >/dev/null 2>&1; then
+            wget -P /tmp https://raw.githubusercontent.com/sam0402/ArchQ/main/pkg/mimalloc-3.5.3-1-x86_64.pkg.tar.zst
+            pacman -U --noconfirm /tmp/mimalloc-3.5.3-1-x86_64.pkg.tar.zst
+        fi
+        for ((i=0; i < ${#arrList[@]}; i++))
+        do
+            menu+=$i' '${arrList[$i]}
+            grep -q MIMALLOC_ALLOW_LARGE_OS_PAGES "${serpath}${arrService[$i]}.service" && menu+=' on ' || menu+=' off '
+        done
+
+        options=$(dialog --stdout --title "ArchQ $1" --checklist "HugePages Enable" 7 0 0 ${menu}) || exit 1; clear
+        for ((i=0; i < ${#arrList[@]}; i++))
+        do
+            if ( echo $options | grep -q $i ); then
+                grep -q MIMALLOC_ALLOW_LARGE_OS_PAGES "${serpath}${arrService[$i]}.service" || \
+                sed -i '/User=/i \
+Environment="LD_PRELOAD=/usr/lib/libmimalloc.so.3.5"\
+Environment="MIMALLOC_ALLOW_LARGE_OS_PAGES=1"\
+Environment="MIMALLOC_EAGER_COMMIT_DELAY=0"' "${serpath}${arrService[$i]}.service"
+                sed -i 's/^User=.*/User=root/' "${serpath}${arrService[$i]}.service"
+                sed -i 's|ExecStart=/usr/bin/pagecache-management.sh |ExecStart=|' "${serpath}${arrService[$i]}.service"
+            else
+                sed -i '/Environment=/d' "${serpath}${arrService[$i]}.service"
+            fi
+        done
+    ;;
+esac
 systemctl daemon-reload             
