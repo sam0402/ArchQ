0db3c4b76271b054db6783202e0ce45d27995b8d
sam0402 <sam0402@gmail.com>
2026-09-24 14:30:42 +0800

    Update kernel-cfg.sh

--- config/kernel-cfg.sh
+++ config/kernel-cfg.sh
@@ -16,7 +16,7 @@ pacman -Q xf86-video-fbdev >/dev/null 2>&1 && alsa='' || alsa='A ALSAlib '
 grep -q Hugetlb /proc/meminfo && alsa+='H HugePages '
 
 WK=$(dialog --stdout --title "ArchQ $1" \
-            --menu "Select an action:" 7 0 0 B Boot I Install M Remove F Frequency "$alsa") || exit 1; clear
+            --menu "Select an action:" 7 0 0 B Boot I Install M Remove F Frequency $alsa) || exit 1; clear
 
 mkgrub(){
     if lsblk -pln -o name,partlabel | grep -q Microsoft; then
